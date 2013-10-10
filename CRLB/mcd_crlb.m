302d7379c293a741b528491f8f62fe44769464d3
Samuel A. Hurley <samuel.hurley@gmail.com>
2013-10-09 21:50:32 -0500

    Added script for Cramar-Rao Lower Bound Analysis

---
+++ CRLB/mcd_crlb.m
@@ -0,0 +1,8 @@
+%% SCRIPT mcd_crlb
+%
+%  SCRIPT to compute mcDESPOT Cramar-Rao-Lower-Bounds
+%  for a given acquisition and mcd_fv (parameter set theta)
+%  and given noise levels sigma_spgr and sigma_ssfp
+%
+%  SAH 9-Oct-2013
+
